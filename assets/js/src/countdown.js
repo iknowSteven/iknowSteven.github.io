5b66c9ba0878ebbe7f5f073b3a6c7983cb73ebeb
StevenAhn <c.s.ahn@pillowstudio.co.kr>
2019-02-03 23:04:47 +0900

    no message

--- assets/js/src/countdown.js
+++ assets/js/src/countdown.js
@@ -3,6 +3,7 @@ var countDownDate = new Date(countDownSince+" 09:00:00").getTime();
 var now = new Date().getTime();
 var distance = now - countDownDate;
 var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-
+if(distance<0)
+    document.getElementsByClassName("countdown_remain")[0].innerHTML = "Ready";
 document.getElementsByClassName("countdown_remain")[0].innerHTML = "Since "+days+" days ";
 
