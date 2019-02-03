da433c54f1d152256bec227484ef720f80ac5d00
StevenAhn <c.s.ahn@pillowstudio.co.kr>
2019-02-03 17:34:37 +0900

    no message

--- assets/js/src/countdown.js
+++ assets/js/src/countdown.js
@@ -8,5 +8,5 @@ var x = setInterval(function() {
     var distance = countDownDate - now;
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-    document.getElementsByClassName("countdown_remain").innerHTML = days + "일 ";
+    document.getElementsByClassName("countdown_remain")[0].innerHTML = days + "일 ";
 }, 1000);
