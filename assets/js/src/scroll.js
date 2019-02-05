2cca6a679b87ab576b736bd67f02e445ab621f5d
StevenAhn <c.s.ahn@pillowstudio.co.kr>
2019-02-05 18:47:53 +0900

    no message

--- assets/js/src/scroll.js
+++ assets/js/src/scroll.js
@@ -7,18 +7,18 @@ $(window).resize(function() {
     windowSize = window.innerHeight;
 });
 
-$(window).scroll(function(event) {
-    didScroll = true;
-});
+// $(window).scroll(function(event) {
+//     didScroll = true;
+// });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 1);
+// setInterval(function() {
+//     if (didScroll) {
+//         hasScrolled();
+//         didScroll = false;
+//     }
+// }, 1);
 
-function hasScrolled() {
+$('html').scroll(function() {
     currentScroll = $(this).scrollTop(); //get current
 
     if (currentScroll > lastScroll && st <= windowSize) {
@@ -37,6 +37,28 @@ function hasScrolled() {
     }
 
     lastScrollTop = st;
-}
+})
+
+//
+// function hasScrolled() {
+//     currentScroll = $(this).scrollTop(); //get current
+//
+//     if (currentScroll > lastScroll && st <= windowSize) {
+//         $('html, body').animate({
+//             scrollTop: $('#all_countdown_container').offset().top
+//         }, 1000);
+//
+//     } else if (currentScroll > lastScroll && st > windowSize){ // Scroll is out from the first section and Down
+//         return;
+//     } else if (currentScroll < lastScroll && st == windowSize){ // Scroll is in the first section and Up
+//         $('html, body').animate({
+//             scrollTop: $('#home_main_container').offset().top
+//         }, 1000);
+//     } else if (currentScroll < lastScroll && st > windowSize){ // Scroll is out from the first section and Up
+//         document.getElementById('home_main_container').scrollIntoView({behavior: 'smooth'});
+//     }
+//
+//     lastScrollTop = st;
+// }
 
 
