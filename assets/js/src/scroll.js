3373b601f92927608b2dee192e5e1a17a7d1b1ad
StevenAhn <c.s.ahn@pillowstudio.co.kr>
2019-02-05 18:43:06 +0900

    no message

--- assets/js/src/scroll.js
+++ assets/js/src/scroll.js
@@ -1,13 +1,13 @@
 var didScroll;
-var lastScrollTop = $(this).scrollTop();
+var lastScrollTop = 0;
 var windowSize = window.innerHeight;
 var st;
 
-$(window).resize(function(){
+$(window).resize(function() {
     windowSize = window.innerHeight;
 });
 
-$(window).scroll(function(event){
+$(window).scroll(function(event) {
     didScroll = true;
 });
 
@@ -19,12 +19,22 @@ setInterval(function() {
 }, 250);
 
 function hasScrolled() {
-    st = $(this).scrollTop();
+    st = $(this).scrollTop(); //get current
 
-    if (st > lastScrollTop && st <= windowSize){ // Scroll is in the first section and Down
+    if (st > lastScrollTop && st <= windowSize) {
         $('html, body').animate({
             scrollTop: $('#all_countdown_container').offset().top
         }, 1000);
+
+    } else if (st > lastScrollTop && st > windowSize){ // Scroll is out from the first section and Down
+        return;
+    } else if (st < lastScrollTop && st == windowSize){ // Scroll is in the first section and Up
+        $('html, body').animate({
+            scrollTop: $('#home_main_container').offset().top
+        }, 1000);
+    } else if (st < lastScrollTop && st > windowSize){ // Scroll is out from the first section and Up
+        document.getElementById('home_main_container').scrollIntoView({behavior: 'smooth'});
+    }
 
     lastScrollTop = st;
 }
