8d6bdcc719effb4e70b986969df0f5616c11552c
StevenAhn <c.s.ahn@pillowstudio.co.kr>
2019-02-06 00:32:56 +0900

    no message

--- assets/js/src/scroll.js
+++ assets/js/src/scroll.js
@@ -7,12 +7,20 @@ $(window).resize(function() {
 });
 
 window.addEventListener('scroll', function() {
-    if(window.scrollY < windowSize) {
-        $('#home_main_container').animate({
-            scrollTop: $('#all_countdown_container').offset().top
-        }, 1000);
-    }
-    else return;
+    currentScroll = $(this).scrollTop(); //get current
+        if (currentScroll < lastScroll && currentScroll < windowSize) { // Down Scroll and in the first section
+            $('html, body').animate({
+                scrollTop: $('#all_countdown_container').offset().top
+            }, 1000);
+        } else if (currentScroll  > lastScroll && currentScroll == windowSize ) {
+            $('html, body').animate({
+                scrollTop: $('#home_main_container').offset().top
+            }, 1000);
+        } else if (currentScroll > lastScroll && lastScroll < windowSize && currentScroll >= windowSize) {
+            $('html, body').animate({
+                scrollTop: $('#home_main_container').offset().top
+            }, 1000);
+        }
 });
 
 // var didScroll;
