e60a4a731ed85354a113ff884c2d0231e4fc10ac
StevenAhn <c.s.ahn@pillowstudio.co.kr>
2019-02-06 09:33:34 +0900

    no message

---
+++ assets/js/src/countdown.js
@@ -0,0 +1,9 @@
+var countDownSince = document.getElementsByClassName("countdown_since")[0].innerHTML;
+var countDownDate = new Date(countDownSince+" 00:00:00").getTime();
+var now = new Date().getTime();
+var distance = now - countDownDate;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+if(distance<0)
+    document.getElementsByClassName("countdown_remain")[0].innerHTML = "Ready";
+document.getElementsByClassName("countdown_remain")[0].innerHTML = "Since "+days+" days ";
+
---
+++ assets/js/src/scroll.js
@@ -0,0 +1,115 @@
+var didScroll;
+var currentScroll;
+var lastScroll = 0;
+var windowSize = window.innerHeight;
+
+$(window).scroll(function(event) {
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 100);
+
+$(window).resize(function() {
+    windowSize = window.innerHeight;
+});
+
+function hasScrolled() {
+    currentScroll = $(this).scrollTop(); //get current
+
+    if (currentScroll > lastScroll && lastScroll < windowSize) { // Down Scroll and in the first section
+        $('html, body').animate({
+            scrollTop: $('#all_countdown_container').offset().top
+        }, 1000);
+        lastScroll = currentScroll;
+        return;
+    }
+
+    lastScroll = currentScroll;
+}
+
+
+// window.addEventListener('scroll', function() {
+//     currentScroll = $(this).scrollTop(); //get current
+//     if (currentScroll > lastScroll && lastScroll < windowSize) { // Down Scroll and in the first section
+//         $('html, body').animate({
+//             scrollTop: $('#all_countdown_container').offset().top
+//         }, 1000);
+//         lastScroll = currentScroll;
+//         return;
+//     }
+//     // } else if (currentScroll < lastScroll && currentScroll < 2 * windowSize && currentScroll >= windowSize) {
+//     //     $('html, body').animate({
+//     //         scrollTop: $('#home_main_container').offset().top
+//     //     }, 1000);
+//     // }
+// });
+
+// var didScroll;
+// var currentScroll;
+// var lastScroll = 0;
+// var windowSize = window.innerHeight;
+//
+// $(window).resize(function() {
+//     windowSize = window.innerHeight;
+// });
+
+// $(window).scroll(function(event) {
+//     didScroll = true;
+// });
+
+// setInterval(function() {
+//     if (didScroll) {
+//         hasScrolled();
+//         didScroll = false;
+//     }
+// }, 1);
+
+// $(window).scroll(function() {
+//     currentScroll = $(this).scrollTop(); //get current
+//
+//     if (currentScroll > lastScroll && st <= windowSize) {
+//         $('html, body').animate({
+//             scrollTop: $('#all_countdown_container').offset().top
+//         }, 1000);
+//
+//     } else if (currentScroll > lastScroll && st > windowSize){ // Scroll is out from the first section and Down
+//         return;
+//     } else if (currentScroll < lastScroll && st == windowSize){ // Scroll is in the first section and Up
+//         $('html, body').animate({
+//             scrollTop: $('#home_main_container').offset().top
+//         }, 1000);
+//     } else if (currentScroll < lastScroll && st > windowSize){ // Scroll is out from the first section and Up
+//         document.getElementById('home_main_container').scrollIntoView({behavior: 'smooth'});
+//     }
+//
+//     lastScrollTop = st;
+// })
+
+//
+// function hasScrolled() {
+//     currentScroll = $(this).scrollTop(); //get current
+//
+//     if (currentScroll > lastScroll && st <= windowSize) {
+//         $('html, body').animate({
+//             scrollTop: $('#all_countdown_container').offset().top
+//         }, 1000);
+//
+//     } else if (currentScroll > lastScroll && st > windowSize){ // Scroll is out from the first section and Down
+//         return;
+//     } else if (currentScroll < lastScroll && st == windowSize){ // Scroll is in the first section and Up
+//         $('html, body').animate({
+//             scrollTop: $('#home_main_container').offset().top
+//         }, 1000);
+//     } else if (currentScroll < lastScroll && st > windowSize){ // Scroll is out from the first section and Up
+//         document.getElementById('home_main_container').scrollIntoView({behavior: 'smooth'});
+//     }
+//
+//     lastScrollTop = st;
+// }
+
+
